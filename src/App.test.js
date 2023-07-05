31236de602e834358bc244f6a159de53f6cf21c3
Daniel Rivera <dani_wooten@hotmail.com>
2023-07-04 21:24:45 -0700

    Connected to API

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import BookingPage from '../src/components/BookingPage';
+import { initializeTimes, updateTimes } from '../src/components/BookingPage';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('Renders the BookingForm heading', () => {
+  render(<BookingPage />);
+  const headingElement = screen.getByText('RESERVATIONS');
+  expect(headingElement).toBeInTheDocument();
+});
+
+describe('initializeTimes', () =>{
+  it('function returns an array of times', () => {
+    const expectedTimes = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    const times = initializeTimes();
+    expect(times).toEqual(expectedTimes);
+  });
+});
+
+describe('updateTimes', () =>{
+  it('function returns the same value provide in the state', () => {
+    const currentState = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    const selectedDate = '2023-07-04';
+    const times = updateTimes(selectedDate, currentState);
+    expect(times).toEqual(currentState);
+  });
 });
--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,15 +1,16 @@
 import React, { useReducer } from 'react';
 import BookingForm from './BookingForm';
 
+export const initializeTimes = () => {
+  return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+};
+
+export const updateTimes = (selectedDate) => {
+  return initializeTimes();
+};
+
+
 const BookingPage = () => {
-  const initializeTimes = () => {
-    return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
-  };
-
-  const updateTimes = (selectedDate) => {
-    return initializeTimes();
-  };
-
   const [availableTimes, dispatch] = useReducer(updateTimes, '', initializeTimes);
 
   return (
